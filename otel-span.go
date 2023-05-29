85e8faa0bc7270195357ca919b42b9b23ae9a8b0
yjy86868 <yjy86868@163.com>
2023-05-29 10:19:11 +0800

    Init Commit

---
+++ otel-span.go
@@ -0,0 +1,43 @@
+package Jgrpc_otelspan
+
+import (
+	"context"
+	"errors"
+	"runtime"
+
+	"go.opentelemetry.io/otel/attribute"
+	otelcodes "go.opentelemetry.io/otel/codes"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
+)
+
+// OtelSpan 链路追踪
+type OtelSpan struct {
+	tracer *sdktrace.TracerProvider
+}
+
+// NewOtelSpan 实例化 OtelSpan
+func NewOtelSpan(tracer *sdktrace.TracerProvider) *OtelSpan {
+	return &OtelSpan{
+		tracer: tracer,
+	}
+}
+
+func (s *OtelSpan) Record(ctx context.Context, tracerName string) (context.Context, trace.Span) {
+	pc := make([]uintptr, 1)
+	runtime.Callers(2, pc)
+	f := runtime.FuncForPC(pc[0])
+	return s.tracer.Tracer(tracerName).Start(ctx, f.Name())
+}
+
+// Error 记录链路错误
+// 包含 文件名以及完整路径/错误行数
+func (s *OtelSpan) Error(span trace.Span, msg string) error {
+	_, file, line, _ := runtime.Caller(1)
+	span.SetStatus(otelcodes.Error, msg)
+	span.SetAttributes(
+		attribute.String("file.name", file),
+		attribute.Int("file.line", line),
+	)
+	return errors.New("msg")
+}
